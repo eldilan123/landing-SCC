a849861f0eafde83b266c457a1643e263713b6cb
Dilan Felipe Asencio Perez <dilanfelipeasencioperez@MacBook-Pro-de-Jess.local>
2025-04-22 07:43:39 -0500

    fixing favicon

---
+++ .next/static/chunks/src_app_page_tsx_1cd6a0d9._.js
@@ -0,0 +1,17 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_1cd6a0d9._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_1cd6a0d9._.js",
+  "chunks": [
+    "static/chunks/[root of the server]__95435677._.js",
+    "static/chunks/node_modules_next_f62bd096._.js",
+    "static/chunks/node_modules_@fortawesome_free-solid-svg-icons_index_mjs_6d4e8d45._.js",
+    "static/chunks/node_modules_framer-motion_dist_es_d12c975d._.js",
+    "static/chunks/node_modules_motion-dom_dist_es_86ae3a86._.js",
+    "static/chunks/node_modules_d6951009._.js",
+    "static/chunks/[root of the server]__f33126f5._.css"
+  ],
+  "source": "dynamic"
+});
